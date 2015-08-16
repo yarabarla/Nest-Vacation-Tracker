314dd381b1a9a670e169378f77fc9aec805e0512
Grant Timmerman <granttimmerman@gmail.com>
2015-08-16 01:01:08 -0700

    Add cron job

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -28,9 +29,9 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -38,24 +39,48 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
+// Run every so often
+var CRON_FREQUENCY = 5000;
+setInterval(function() {
+  fs = require('fs')
+  fs.readFile('./data/flights.json', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    var flights = JSON.parse(data).flights;
+
+    // Check if the flight is now
+    for (var i in flights) {
+      var flight = flights[i];
+      var now = +new Date();
+      if (flight.start_time > now && flight.start_time <= now + CRON_FREQUENCY) {
+        // Flight is now, go message about it.
+        request('http://localhost:3000');
+      } else if (flights.end_time - CRON_FREQUENCY >  now && flights.end_time < now) {
+        // Flight came back now, go message about it.
+        request('http://localhost:3000');
+      }
+    }
+  });
+}, CRON_FREQUENCY);
 
 module.exports = app;
