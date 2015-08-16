fe83a520d7c816cd2b8dddff0d9346edec5595cb
dhalima3 <dhalima@gatech.edu>
2015-08-16 09:52:22 -0700

    route for UI

--- nestUI.js
+++ nestUI.js
@@ -6,27 +6,18 @@
 // ==/UserScript==
 
 $(document).ready(function() {
-    //assume equipment size is 3 tons per household
-    var equipmentSize = 3;
-    //average electric rate for the US
-    var averageElectricRate = 17.5;
-    //hours per day unit is on
-    var hoursPerDay = 5;
-    //SEER efficiency rating
-    var seer = 10;
-    var days = 31;
-    var cost = Math.round((days*averageElectricRate*hoursPerDay) / equipmentSize / seer * 100)/100;
-
-    var html = '<div class="price-saver" style="color:white">' +
-        '<h2 class="mode-title">' +
-        'You have saved $' + cost + ' from turning off your HVAC during your vacation! ' +
-        'Now go treat yourself!' +
-        '</h2> </div>';
-
-    var interval = setInterval(function () {
-        if ($(".add-device-deck-item").length) {
-            clearInterval(interval);
-            $(html).insertBefore("li.add-device-deck-item");
-        }
-    });
+    $.get('/api', function(data) {
+        var html = '<div class="price-saver" style="color:white">' +
+            '<h2 class="mode-title">' +
+    'You have saved $<strong>' + data + '</strong> from turning off your HVAC during your vacation! ' +
+            'Now go treat yourself!' +
+            '</h2> </div>';
+      
+        var interval = setInterval(function () {
+            if ($(".add-device-deck-item").length) {
+                clearInterval(interval);
+                $(html).insertBefore("li.add-device-deck-item");
+            }
+        });
+    }
 });
