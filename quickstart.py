bc6fb3c0622bb9f944d557cf8d65fafb6e6c827f
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-08-15 21:00:46 -0500

    Updated main call

--- quickstart.py
+++ quickstart.py
@@ -45,6 +45,12 @@ def get_credentials():
         print 'Storing credentials to ' + credential_path
     return credentials
 
+"""Get a list of Messages from the user's mailbox.
+"""
+
+from apiclient import errors
+
+
 def ListMessagesMatchingQuery(service, user_id, query=''):
   """List all Messages of the user's mailbox matching the query.
 
@@ -77,6 +83,102 @@ def ListMessagesMatchingQuery(service, user_id, query=''):
   except errors.HttpError, error:
     print 'An error occurred: %s' % error
 
+
+def ListMessagesWithLabels(service, user_id, label_ids=[]):
+  """List all Messages of the user's mailbox with label_ids applied.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    label_ids: Only return Messages with these labelIds applied.
+
+  Returns:
+    List of Messages that have all required Labels applied. Note that the
+    returned list contains Message IDs, you must use get with the
+    appropriate id to get the details of a Message.
+  """
+  try:
+    response = service.users().messages().list(userId=user_id,
+                                               labelIds=label_ids).execute()
+    messages = []
+    if 'messages' in response:
+      messages.extend(response['messages'])
+
+    while 'nextPageToken' in response:
+      page_token = response['nextPageToken']
+      response = service.users().messages().list(userId=user_id,
+                                                 labelIds=label_ids,
+                                                 pageToken=page_token).execute()
+      messages.extend(response['messages'])
+
+    return messages
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error    
+
+
+"""Get Message with given ID.
+"""
+
+import base64
+import email
+from apiclient import errors
+
+def GetMessage(service, user_id, msg_id):
+  """Get a Message with given ID.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    msg_id: The ID of the Message required.
+
+  Returns:
+    A Message.
+  """
+  try:
+    message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+
+    print 'Message snippet: %s' % message['payload']
+
+    return message
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+
+def GetMimeMessage(service, user_id, msg_id):
+  """Get a Message and use it to create a MIME Message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    msg_id: The ID of the Message required.
+
+  Returns:
+    A MIME Message, consisting of data from Message.
+  """
+  try:
+    message = service.users().messages().get(userId=user_id, id=msg_id,
+                                             format='raw').execute()
+
+    print 'Message snippet: %s' % message['snippet']
+
+    msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
+
+    mime_msg = email.message_from_string(msg_str)
+    messageMainType = mime_msg.get_content_maintype()
+    if messageMainType == 'multipart':
+        for part in mime_msg.get_payload():
+            if part.get_content_maintype() == 'text':
+                return part.get_payload()
+        return ""
+    elif messageMainType == 'text':
+        return mime_msg.get_payload()
+
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error    
+
 def main():
     """Shows basic usage of the Gmail API.
 
@@ -88,15 +190,25 @@ def main():
     service = discovery.build('gmail', 'v1', http=http)
 
     results = service.users().labels().list(userId='me').execute()
-    print ListMessagesMatchingQuery(service, "akhileshyarabarla@gmail.com", "from:flightstatus@t.spiritairlines.com")
-    """labels = results.get('labels', [])
+    messageIds = ListMessagesMatchingQuery(service, 'me', query='subject:Flight Confirmation')
 
-    if not labels:
-        print 'No labels found.'
-    else:
-      print 'Labels:'
-      for label in labels:
-        print label['name']"""
+    datalist = []
+    times = []
+    outputTxt = open("out.txt", "w")
+    for message in messageIds:
+        datalist.append(GetMimeMessage(service, 'me', message[u'id']))
+        messageX = datalist[-1]
+        outputTxt.write("New Message: ----")
+        outputTxt.write(messageX)
+    #print message
+    #labels = results.get('labels', [])
+
+    #if not labels:
+        #print 'No labels found.'
+    #else:
+      #print 'Labels:'
+      #for label in labels:
+        #print label['name']
 
 
 if __name__ == '__main__':
